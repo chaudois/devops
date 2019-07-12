7ec1369ac1378b99b2f9b99819db411e55cd4e70
Chaudois <damien.chaudois@gmail.com>
2019-07-12 14:51:23 +0200

    .

---
+++ BlogChaudois/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogChaudois.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
